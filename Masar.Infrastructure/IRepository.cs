2044aa2c54590abd6db9581a766e4b731b040b70
Nadakhaled34 <khalednada356@gmail.com>
2026-03-15 22:57:33 +0200

    Added Dtos

---
+++ Masar.Infrastructure/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masar.Infrastructure
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+    }
+}
